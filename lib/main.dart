c6aa3d7c518416b14cf539c43690a7293650682f
shabake <45329453@qq.com>
2020-06-23 12:41:41 +0800

    完善

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'GHFilterDemo.dart';
 
-
 void main() => runApp(MyApp());
 
 
---
+++ lib/services/ScreenAdaper.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ScreenAdaper {
+  static init(context) {
+    ScreenUtil.init(width: 750, height: 1334);
+  }
+
+  static height(double value) {
+    return ScreenUtil().setHeight(value);
+  }
+
+  static width(double value) {
+    return ScreenUtil().setWidth(value);
+  }
+
+  static getScreenHeight() {
+    return ScreenUtil.screenHeight;
+  }
+
+  static getScreenWidth() {
+    return ScreenUtil.screenWidth;
+  }
+}
